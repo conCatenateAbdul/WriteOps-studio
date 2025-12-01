997aa89333ad86c52e29cdaf0a055ab8bf1dbee4
Uthman Abdulmalik <57192819+conCatenateAbdul@users.noreply.github.com>
2025-12-01 10:45:32 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/blog/2025-11-29- introduction">
+            to="/blog/2025-11-29-introduction">
             Introduction - 3min ⏱️
           </Link>
         </div>
