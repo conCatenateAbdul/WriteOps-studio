92e94331b8d9185789a1904e4af3089b51d2ebb2
Uthman Abdulmalik <57192819+conCatenateAbdul@users.noreply.github.com>
2025-12-05 15:14:48 +0100

    Delete src/components/ClassyText.tsx

--- src/components/ClassyText.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from 'react';
-
-interface ClassyTextProps {
-    children: ReactNode;
-}
-
-export default function ClassyText({ children }: ClassyTextProps) {
-    return <div className="classy-text">{children}</div>;
-}
