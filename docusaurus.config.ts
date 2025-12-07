091eefc71d16a142113df4b8e79a9ff7da93d166
Uthman Abdulmalik <57192819+conCatenateAbdul@users.noreply.github.com>
2025-12-07 12:21:28 +0100

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'WriteOps-Studio',
   tagline: 'Technical Writer | IT Support Specialist | Cybersecurity Analyst',
-  favicon: 'img/favicon.svg', 
+  favicon: 'img/favicon1.png', 
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
